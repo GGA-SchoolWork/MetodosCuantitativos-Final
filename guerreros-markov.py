bd73a2e7eb39cb7e1184adbe4ed9797533fff2cf
Gerardo Granados <gerardogr.al@outlook.com>
2022-11-25 02:39:33 -0600

    Initial commit

---
+++ guerreros-markov.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def crear_matriz(n: int):
+	"""Crea matriz estocastica para tamaño n"""
+	return
+
+def actualizar_matriz():
+	"""Actualiza matriz y elimina un grupo"""
+	return
+
+def guerreros_markov():
+	"""Correr codigo"""
+	return
+
+if __name__ == "__main__":
+	guerreros_markov()
